455b40ce501bdd01380b871155afc1509aeb77bf
Ashu8408 <92422960+Ashu8408@users.noreply.github.com>
2024-05-29 22:47:12 +0530

    Time: 4 ms (45.20%), Space: 8.1 MB (35.35%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         int n = s.size();
-        std::vector<int> result(n, n);  // Initialize distances with a large number (n)
-
-        // First pass: left to right
-        int position = -n;  // A position far to the left
+        std::vector<int> result(n, n);  
+        
+        int position = -n;  
         for (int i = 0; i < n; ++i) {
             if (s[i] == c) {
                 position = i;
             }
             result[i] = std::min(result[i], std::abs(i - position));
         }
+
+        position = 2 * n; 
         for (int i = n - 1; i >= 0; --i) {
             if (s[i] == c) {
                 position = i;
