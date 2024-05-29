3bb14e55f3434b9f9bde5a49b00de35b77d4dd83
Ashu8408 <92422960+Ashu8408@users.noreply.github.com>
2024-05-29 23:00:29 +0530

    Time: 4 ms (45.20%), Space: 8 MB (70.82%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -5,19 +5,18 @@ public:
         std::vector<int> result(n, n);  
         
         int position = -n;  
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i<n; i++) {
             if (s[i] == c) {
                 position = i;
             }
-            result[i] = std::min(result[i], std::abs(i - position));
+            result[i] = min(result[i], abs(i - position));
         }
 
-        position = 2 * n; 
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = n-1; i>=0; i--) {
             if (s[i] == c) {
                 position = i;
             }
-            result[i] = std::min(result[i], std::abs(i - position));
+            result[i] = min(result[i], abs(i - position));
         }
 
         return result;
